10ee0021a4a5562369c2487d5d3e488a3d8e0bfa
Everson <eversonpereiraofc@gmail.com>
2022-03-08 16:34:30 -0300

    criando componente formulário

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -5,7 +5,7 @@ interface BotaoProps{
 }
 
 export default function Botao(props: BotaoProps){
-    const cor = props.cor ?? 'blue'
+    const cor = props.cor ?? 'gray'
     console.log(cor)
     return (
         <button className={`bg-gradient-to-r from-${cor}-400 to-${cor}-700 text-white px-4 py-2 rounded-md ${props.className}`}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "../components/Layout"
 import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
 import Botao from "../components/Botao"
+import Formulario from "../components/Formulario"
 
 export default function Home() {
   const clientes = [
@@ -21,12 +22,17 @@ export default function Home() {
   
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
-      <Layout titulo="Cadastro Simples" >
+      { /* <Layout titulo="Cadastro Simples" >
         <div className="flex justify-end">
           <Botao cor="green" className="mb-2">Novo Cliente</Botao>
         </div>
         <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+      </Layout> */}
+
+      <Layout titulo="Cadastro Simples">
+        <Formulario cliente={clientes[2]}></Formulario>
       </Layout>
+      
     </div>
   )
 }
