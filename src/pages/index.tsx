0ad0c122ff3d136482991c6f0bf22b9c2348e201
Everson <eversonpereiraofc@gmail.com>
2022-03-08 15:31:30 -0300

    criando componente botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout"
 import Tabela from "../components/Tabela"
 import Cliente from "../core/Cliente"
+import Botao from "../components/Botao"
 
 export default function Home() {
   const clientes = [
@@ -11,16 +12,19 @@ export default function Home() {
   ]
 
   function clienteSelecionado(cliente: Cliente){
-
+    console.log(cliente.nome)
   }
 
   function clienteExcluido(cliente: Cliente){
-
+    console.log(cliente.nome + " excluído(a)")
   }
   
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples" >
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-2">Novo Cliente</Botao>
+        </div>
         <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
       </Layout>
     </div>
