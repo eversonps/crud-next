a48066b4c457e35b4677be8562cb7f86365e5b95
Everson <eversonpereiraofc@gmail.com>
2022-03-08 14:55:15 -0300

    criando classe cliente e o componente tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Marcos", 18, "1"),
+    new Cliente("Ana", 28, "2"),
+    new Cliente("Leandro", 19, "3"),
+    new Cliente("Daniel", 24, "4")
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+
+  }
+
+  function clienteExcluido(cliente: Cliente){
+
+  }
+  
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples" >
-        <span>Conteúdo</span>  
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
       </Layout>
     </div>
   )
